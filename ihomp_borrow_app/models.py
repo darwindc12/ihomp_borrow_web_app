3c71fffa591512b611a4ddd83e54c984d7f2da7e
Darwin Dela Cruz <darwindelacruz0012@gmail.com>
2023-10-21 23:53:25 +0800

    Create database for models.py

--- ihomp_borrow_app/models.py
+++ ihomp_borrow_app/models.py
@@ -1,3 +1,66 @@
 from django.db import models
 
-# Create your models here.
+
+STATUS = (
+    (0, "Unavailable"),
+    (1, "Available")
+)
+
+
+class Category(models.Model):
+    category_id = models.AutoField(primary_key=True)
+    category_description = models.CharField(max_length=2000)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.category_description
+
+
+class Peripheral(models.Model):
+    peripheral_id = models.AutoField(primary_key=True)
+    peripheral_description = models.CharField(max_length=2000)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    unique_number = models.CharField(max_length=2000, default=0000)
+    status = models.IntegerField(choices=STATUS, default=1)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.peripheral_description
+
+
+class Department(models.Model):
+    department_id = models.AutoField(primary_key=True)
+    department_description = models.CharField(max_length=2000)
+    status = models.IntegerField(choices=STATUS, default=1)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.department_description
+
+
+
+class Borrow(models.Model):
+    borrow_id = models.AutoField(primary_key=True)
+    borrower_name = models.CharField(max_length=2000)
+    department = models.ForeignKey(Department, on_delete=models.PROTECT)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    peripheral = models.ForeignKey(Peripheral, on_delete=models.PROTECT)
+    unique_number = models.CharField(max_length=2000, blank=True)
+    status = models.IntegerField(choices=STATUS, default=1)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.borrower_name
+
+    def save(self, *args, **kwargs):
+        # Check if the unique_number field is empty and if it's the first save (not an update)
+        if not self.unique_number and not self.pk:
+            self.unique_number = self.peripheral.unique_number
+        super(Borrow, self).save(*args, **kwargs)
+
+
+
